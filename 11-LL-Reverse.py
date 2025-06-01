2c92fc1b53e1c75e20d613412f975ed2fb594c98
Mark Gomez <mark.g.gomez@protonmail.com>
2025-06-01 16:32:12 -0700

    adding exce 11

--- 11-LL-Reverse.py
+++ 11-LL-Reverse.py
@@ -171,14 +171,35 @@ class LinkedList:
         self.length -= 1
         return temp
 
-my_linked_list = LinkedList(11)
-my_linked_list.append(3)
-my_linked_list.append(23)
-my_linked_list.append(7)  
+# my_linked_list = LinkedList(11)
+# my_linked_list.append(3)
+# my_linked_list.append(23)
+# my_linked_list.append(7)  
 
-print(my_linked_list.remove(2), '\n')
+# print(my_linked_list.remove(2), '\n')
+
+# my_linked_list.print_list()
+
+
+# ------------------11 Reverse---------------------
+
+    def reverse(self):
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+        after = temp.next
+        before = None
+        for _ in range(self.length):
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
+
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+my_linked_list.append(4)
+
+my_linked_list.reverse()
 
 my_linked_list.print_list()
-
-
-        
